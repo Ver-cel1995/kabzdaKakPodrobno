f976cf7cc10923a4a3ab80aaf36d6b2f37514913
incubator_alex <aleksei.tselikin@gmail.com>
2024-08-13 11:11:18 +0300

    Resolved merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import UncontrolledRating from "./01/UncontrolledRating";
+import Accordion from "./01/Accordion";
+import Rating, {RatingValueType} from "./01/Rating";
+import UncontrolledAccordion from "./01/UncontrolledAccordion";
+import {OnOff} from "./02/OnOff";
 
 function App() {
+
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(3)
+
+
+    const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/*<Accordion titleValue={"Menu"} collapsed={true}/>*/}
+        {/*<Accordion titleValue={"Users"} collapsed={false}/>*/}
+        {/*<UncontrolledRating />*/}
+        <OnOff/>
+        <UncontrolledAccordion titleValue={'menu'} collapsed={collapsed} onChange={ () => setCollapsed(!collapsed) }/>
+
+        <Rating value={ratingValue} onClickStar={setRatingValue}/>
     </div>
   );
 }
